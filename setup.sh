1dfa8b7a3a06540a4f308dbd12f66ff1b72a7788
JorgeCollinetNavent <jcollinet@navent.com>
2019-01-29 18:07:03 -0300

    agrego dataset ThrityMusic

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 cd data
 wget http://static.echonest.com/millionsongsubset_full.tar.gz
 tar -xvzf millionsongsubset_full.tar.gz
+
+wget https://crossnox.sytes.net/owncloud/index.php/s/edt1ULkBOeQacul/download -O ThirtyMusic.tar.gz
+tar -xvzf ThirtyMusic.tar.gz
